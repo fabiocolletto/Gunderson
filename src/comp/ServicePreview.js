728d7aed33986f581ecd3177d0e1a3d403d30eeb
fabiocolletto <fabiocolletto@gmail.com>
2023-12-12 05:57:39 -0300

    adicionado estrutura do componente servicepreview

--- src/comp/ServicePreview.js
+++ src/comp/ServicePreview.js
@@ -5,10 +5,28 @@ import './ServicePreview.css';
 const ServicePreview = () => {
   const { t } = useTranslation();
 
+  // Caminhos corretos para as imagens de fundo dos cards
+  const cardBackgroundImages = [
+    '/images/service-card1.svg', // Caminho para a imagem do primeiro card
+    '/images/service-card2.svg', // Caminho para a imagem do segundo card
+    '/images/service-card3.svg'   // Caminho para a imagem do terceiro card
+  ];
+
   return (
-    <div className="main-cover">
-      <div className="cover-content">
-        {t('ServicePreview.text')}
+    <div className="service-preview">
+      <h2>{t('ServicePreview.header')}</h2>
+      <div className="cards-container">
+        {cardBackgroundImages.map((image, index) => (
+          <div 
+            className="card" 
+            key={index} 
+            style={{ backgroundImage: `url(${image})` }}
+          >
+            <h3>{t(`ServicePreview.card${index + 1}.title`)}</h3>
+            <p>{t(`ServicePreview.card${index + 1}.text`)}</p>
+            <a href="/service-page">{t('ServicePreview.cardLink')}</a>
+          </div>
+        ))}
       </div>
     </div>
   );
